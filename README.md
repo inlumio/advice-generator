e04b563ca0695c7bc5aabe40b0d898800c1802d0
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-07-06 01:42:22 +0300

    change image import, readme and add gh-pages package

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Advice generator app
 
-This is my version of advice generator. Here I learn how to use `useEffect`
-hook, and Tailwind postcss library in React.
+This is my version of
+[Advice generator](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db)
+challenge on [Frontend Mentor](https://www.frontendmentor.io/). Here I learn how
+to use `useEffect` hook, and Tailwind postcss library in React.
 
 [🚀 **GitHub Pages live preview** 🚀](https://inlumio.github.io/advice-generator/)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import dice from './assets/icon-dice.svg';
+import dividerMobile from './assets/pattern-divider-mobile.svg';
+import dividerDesktop from './assets/pattern-divider-desktop.svg';
 import Advice from './Advice';
 import './App.css';
 import { useState, useEffect } from 'react';
@@ -25,11 +27,8 @@ function App() {
 			<Advice advice={advice} />
 
 			<picture className='divider'>
-				<source
-					media='(min-width: 768px)'
-					srcSet='src/assets/pattern-divider-desktop.svg'
-				/>
-				<img src='src/assets/pattern-divider-mobile.svg' alt='' />
+				<source media='(min-width: 768px)' srcSet={dividerDesktop} />
+				<img src={dividerMobile} alt='' />
 			</picture>
 
 			<button className='advice-search' onClick={fetchAdvice}>
