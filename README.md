2d1045db070d130e468e5d9912976944df3f2a37
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-07-05 23:45:29 +0300

    add fetching data from api, update README and  viteconfig

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advice generator app
+
+This is my version of advice generator. Here I learn how to use `useEffect`
+hook, and Tailwind postcss library in React.
+
+[🚀 **GitHub Pages live preview** 🚀](https://inlumio.github.io/advice-generator/)
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import dice from './assets/icon-dice.svg';
 import Advice from './Advice';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-	const [advice, setAdvice] = useState({
-		number: 114,
-		text: 'It is easyto sit up and take notice, whats difficult is getting up and taking action.',
-	});
+	const [advice, setAdvice] = useState({});
+
+	const fetchAdvice = () => {
+		fetch('https://api.adviceslip.com/advice')
+			.then((response) => response.json())
+			.then((data) =>
+				setAdvice({ number: data.slip.id, text: data.slip.advice })
+			);
+	};
+
+	useEffect(() => {
+		fetchAdvice();
+	}, []);
 
 	// https://api.adviceslip.com
 
@@ -23,7 +32,7 @@ function App() {
 				<img src='src/assets/pattern-divider-mobile.svg' alt='' />
 			</picture>
 
-			<button className='advice-search'>
+			<button className='advice-search' onClick={fetchAdvice}>
 				<img src={dice} alt='' className='advice-dice' />
 			</button>
 		</div>
